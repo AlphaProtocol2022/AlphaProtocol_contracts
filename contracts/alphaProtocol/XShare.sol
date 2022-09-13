384575eb16ed4a3cc38da600ead81927ff929fb1
alpha-master <alpha.protocol.defi@gmail.com>
2022-09-13 09:02:23 +0700

    V-01: Fixed as recommendation

--- contracts/alphaProtocol/XShare.sol
+++ contracts/alphaProtocol/XShare.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.12;
+pragma solidity ^0.6.12;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
